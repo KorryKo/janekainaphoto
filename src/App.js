0fdc0444220c0b32aa07acd47bd66f7dde2821c9
marinadev <maryaleks@mail.ru>
2020-08-24 22:11:56 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardList from '../src/Components/CardList/cardList'
+import Card from './Components/Card/card';
+import Menu from './Components/menu/menu'
 
-function App() {
-  return (
+const cards = [
+  Card, Card, Card, Card, Card
+]
+
+class App extends React.Component {
+  render() { 
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+      <CardList cards={cards} /> 
+
+
     </div>
   );
+  }
 }
 
 export default App;
